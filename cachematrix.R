24dbc291543ac90fa4888a135f90fe6615e0a8fb
nk <kreft.n@gmail.com>
2016-12-17 13:11:26 +0100

    version 1 ready for submit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## INTRODUCTION
+#  The goal of the two functions in this script is to make sure that the inverse
+#  of a matrix is only calculated once and cached for future reference. 
+ 
+## DESCRIPTION
+#  This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## DESCRIPTION
+#  This function computes the inverse of the special "matrix" returned by 
+#  `makeCacheMatrix` above. If the inverse has already been calculated 
+#  (and the matrix has not changed), then `cacheSolve` should retrieve the 
+#  inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
+
+
+#################### TESTING FUNCTIONALITY ####################
+
+#construct matrix object
+mat<-makeCacheMatrix(matrix(c(1,0,0,1),2,2))
+
+#check matrix data
+mat$get()         
+
+#get inverse: not present
+mat$getinverse()
+
+#calculate inverse
+cacheSolve(mat)
+
+#get inverse: inverse present
+mat$getinverse()
+
+#calculate inverse again: lookup from cache :)
+cacheSolve(mat)
+
+#set new matrix on same object
+mat$set(mat<-makeCacheMatrix(matrix(c(1,0,0,-1),2,2)))
+
+# check if inverse is empty again
+mat$getinverse()
+
+#calculate inverse: new inverse is calculated based on new matrix data
+cacheSolve(mat)
+
+
